e73f652f51b0d8ebf4ab7cc43be749c624108326
DineKumar1998 <dineshkumar.webhopers@gmail.com>
2023-02-16 17:45:56 +0530

    added common folder

--- admin-components/adminpanel/adminpanel.js
+++ admin-components/adminpanel/adminpanel.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { MainContainer,HeadContainer,Container,Sidebar} from './styled/admin.styled'
+import React from "react";
+import {
+  MainContainer,
+  HeadContainer,
+  Container,
+  Sidebar,
+} from "./styled/admin.styled";
 
 function Adminpanel() {
   return (
     <>
-    <MainContainer>
+      <MainContainer>
         <HeadContainer> hi</HeadContainer>
         <Container>
-            <Sidebar></Sidebar>
+          <Sidebar></Sidebar>
         </Container>
-    </MainContainer>
+      </MainContainer>
     </>
-  )
+  );
 }
 
-export default Adminpanel
+export default Adminpanel;
--- admin-components/adminpanel/styled/admin.styled.js
+++ admin-components/adminpanel/styled/admin.styled.js
@@ -1,23 +1,23 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-width: 100%;
-height: fit-content;
-display: block;
-`
+  width: 100%;
+  height: fit-content;
+  display: block;
+`;
 export const HeadContainer = styled.div`
-width: 100%;
-height: 50px;
-background-color: grey;
-`
+  width: 100%;
+  height: 50px;
+  background-color: grey;
+`;
 export const Container = styled.div`
-display: flex;
-width: 100%;
-height : 90vh;
-background-color: white;
-`
+  display: flex;
+  width: 100%;
+  height: 90vh;
+  background-color: white;
+`;
 export const Sidebar = styled.div`
-width: 30%;
-height: 90vh;
-background-color: pink;
-`
+  width: 30%;
+  height: 90vh;
+  background-color: pink;
+`;
